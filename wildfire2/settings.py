c926383eaad426eee7caf133f54b77fe88d16bee
Ennyola <medunoyeeni@gmail.com>
2021-06-21 08:10:04 +0100

    setings.py

--- wildfire2/settings.py
+++ wildfire2/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'wildfire2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'wildfire-backend-python1',
+        'USER': 'pythontest',
+        'PASSWORD': 'pdai2@2o21SA',
+        'HOST': '68.183.121.201',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
