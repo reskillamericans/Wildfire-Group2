031880e1473edc55317b84010a7f31d7dd10e31b
olowu abayomi <aayobam@gmail.com>
2021-07-07 18:25:09 +0100

    configuration and staticfiles updates

--- wildfire2/urls.py
+++ wildfire2/urls.py
@@ -1,9 +1,5 @@
-from wildfire2.settings import MEDIA_ROOT
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.static import static
-from django.conf import settings
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from fireapp import views as fireapp_views
 
 
@@ -15,7 +11,3 @@ urlpatterns = [
     path('', fireapp_views.index, name="homepage"),
     path('', include('fireapp.urls')),
 ]
-
-urlpatterns += staticfiles_urlpatterns()
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
