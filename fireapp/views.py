dfb7e2b4057c9f2ad42ea8214e5dfce80b3c68ae
olowu abayomi <aayobam@gmail.com>
2021-07-07 02:06:58 +0100

    Added ADMINs Notifications

--- fireapp/views.py
+++ fireapp/views.py
@@ -4,6 +4,7 @@ from django.core.checks import messages
 from django.conf import settings
 from django.shortcuts import redirect, render
 from .models import Newsletter, Faq
+from django.contrib.auth.models import User
 from .forms import NewsletterForm
 from django.core.mail import send_mail
 from django.contrib import messages
@@ -12,6 +13,7 @@ from django.contrib import messages
 
 # Newsletter view
 def index(request):
+    users = User.objects.filter(is_superuser=True)
     newsletter_form = ""
     template_name = "fireapp/index.html"
     if request.method == "POST":
@@ -30,9 +32,18 @@ def index(request):
                 from_email=settings.DEFAULT_FROM_EMAIL,
                 recipient_list=[str(subscriber.email)],
                 fail_silently=False
-                ),
+                )
                 messages.success(request, f"Your email {subscriber.email} has been added to our newsletter successfully.")
+                for user in users:
+                    send_mail(
+                        subject="New Reskill Wildfire Subscriber.",
+                        message="Dear Admin, You have a new Reskill Americans wildfire subscriber.",
+                        from_email=settings.DEFAULT_FROM_EMAIL, 
+                        recipient_list= [user.email],
+                    )
+                    print("Sending mail notification to------->>>", user.email)
                 return redirect("homepage")
+                    
         else:
             messages.warning(request, f"email required")
             return redirect("homepage")
