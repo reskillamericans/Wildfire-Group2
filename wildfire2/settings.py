15cbc34c26039b7ef1ebf1eef46d0414081b2126
Ennyola <medunoyeeni@gmail.com>
2021-07-21 14:45:27 +0100

    added host

--- wildfire2/settings.py
+++ wildfire2/settings.py
@@ -16,8 +16,8 @@ environ.Env.read_env()
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-ALLOWED_HOSTS = []
+DEBUG = False
+ALLOWED_HOSTS = ['wildfire2.herokuapp.com', 'localhost']
 
 
 
