0cf94e1cbb636a3ab159c1989b6513d288c18aae
Ennyola <medunoyeeni@gmail.com>
2021-07-21 07:53:02 +0100

    removed allowed host

--- wildfire2/settings.py
+++ wildfire2/settings.py
@@ -16,11 +16,8 @@ environ.Env.read_env()
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-if not DEBUG:
-    ALLOWED_HOSTS = ['wildfire2.herokuapp.com']
-else:
-    ALLOWED_HOSTS = []
+DEBUG = True
+ALLOWED_HOSTS = []
 
 
 
