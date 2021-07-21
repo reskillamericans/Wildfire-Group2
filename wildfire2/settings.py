19e00bb8d0f05e67ef1472da8a5b901db7f94eb0
Ennyola <medunoyeeni@gmail.com>
2021-07-21 07:27:25 +0100

    changed debug mode

--- wildfire2/settings.py
+++ wildfire2/settings.py
@@ -16,9 +16,11 @@ environ.Env.read_env()
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+DEBUG = False
+if not DEBUG:
+    ALLOWED_HOSTS = ['wildfire2.herokuapp.com','127.0.0.1','localhost']
+else:
+    ALLOWED_HOSTS = []
 
 
 
