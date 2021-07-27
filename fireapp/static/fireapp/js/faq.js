533e7dbcd5cab1253ecf1ceb3b2bccf02effd332
Giulio <riondino.giulio@gmail.com>
2021-07-26 17:53:22 -0700

    Bug fix: faq accordion stopped working post search

--- fireapp/static/fireapp/js/faq.js
+++ fireapp/static/fireapp/js/faq.js
@@ -1,17 +1,18 @@
-let questions = document.getElementsByClassName("questions");
-// console.log("Hello World");
-for (let i = 0; i < questions.length; i++) {
-	questions[i].addEventListener("click", function () {
-		this.classList.toggle("is-open");
-		let content = this.nextElementSibling;
-		if (content.style.maxHeight) {
-			// if truthy set to falsey
-			// set height value to zero or null
-			content.style.maxHeight = null;
-		} else {
-			// if closed (falsey) set to truthy
-			// Set max height === content height on screen
-			content.style.maxHeight = content.scrollHeight + "px";
-		}
-	});
-}
+let questions = document.querySelector(".accordian");
+// targeting the div containing the questions
+
+questions.addEventListener("click", (e) => {
+	// event parameter captures the event
+	e.target.classList.toggle("is-open");
+	//event.target figures out what element within the div was clicked
+	let content = e.target.nextElementSibling;
+	if (content.style.maxHeight) {
+		// if truthy set to falsey
+		// set height value to zero or null
+		content.style.maxHeight = null;
+	} else {
+		// if closed (falsey) set to truthy
+		// Set max height === content height on screen
+		content.style.maxHeight = content.scrollHeight + "px";
+	};
+});
