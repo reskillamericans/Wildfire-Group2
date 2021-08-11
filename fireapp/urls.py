ee8c78fdebb0347d54ad9202a8e17aca673cae08
Ennyola <medunoyeeni@gmail.com>
2021-08-11 14:56:11 +0100

    changed route in app's urls.py file

--- fireapp/urls.py
+++ fireapp/urls.py
@@ -3,6 +3,7 @@ from .views import index, faq, about_us, contact
 
 urlpatterns = [
     path('', index, name="homepage"),
+    path('homepage/', index, name="home"),
     path('faq/', faq, name='faq'),
     path('about-us/', about_us, name="about_us"),
     path('contact/', contact, name="contact"),
