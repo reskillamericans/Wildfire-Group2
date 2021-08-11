b430efde3c38b3ea21629cf5f8b41b2dafc06ba7
Ennyola <medunoyeeni@gmail.com>
2021-08-11 14:43:43 +0100

    changed urls.py

--- wildfire2/urls.py
+++ wildfire2/urls.py
@@ -1,10 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from fireapp import views as fireapp_views
-
-
-admin.site.site_title=" Reskill wildfire"
-admin.site.site_header="Reskill Wildifre Project"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
