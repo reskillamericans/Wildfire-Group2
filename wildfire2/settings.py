1e4579b74a9ec4ac90d334cdaec0bc3b3eea1bf6
Ennyola <medunoyeeni@gmail.com>
2021-08-11 14:20:32 +0100

    debug to false

--- wildfire2/settings.py
+++ wildfire2/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['127.0.0.1', 'wildfire2.herokuapp.com']
 
 
@@ -45,7 +45,7 @@ ROOT_URLCONF = 'wildfire2.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,"templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
